6eae64f78734c6fd2404668941abdbcb2c2c04c0
AlbinaIrb <148874453+AlbinaIrb@users.noreply.github.com>
2024-07-08 03:39:20 +0800

    с тестами близны

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "DetectMerger.h"
 #include <unordered_map>
 #include <cmath>
+#include "main.h"
 
 void addDefect(std::vector<std::vector<BatchResult>>& inputBatchesDefects, int i, int j,
     int x, int y, int width, int height, const std::string& defectClass)
@@ -33,7 +34,7 @@ void addDefect(std::vector<std::vector<BatchResult>>& inputBatchesDefects, int i
     inputBatchesDefects[i][j].detects.push_back(defect);
 }
 
-void addHangingSting(std::vector<std::vector<BatchResult>>& inputBatchesDefects, int i, int j,
+void addLineDefect(std::vector<std::vector<BatchResult>>& inputBatchesDefects, int i, int j,
     int x, int y, int width, int height, const std::string& defectClass, cv::Mat stringMask)
 {
     DetectResult defect;
@@ -181,80 +182,160 @@ int main()
                 //    addDefect(inputBatchesDefects, i, j, 100, 1200, 30, 30, "B.4");
                 //}
 
-                // Висячая нить
-                if (i == 3 && j == 0) {
-                    cv::Mat currentMask1 = cv::Mat::zeros(cv::Size(55, 70), CV_8UC1);
-                    cv::line(currentMask1, cv::Point(0, 70), cv::Point(55, 0), cv::Scalar(255), 3);
-                    addHangingSting(inputBatchesDefects, i, j, 200, 1500, 55, 70, "O.2.3", currentMask1);
+                //// Висячая нить
+                //if (i == 3 && j == 0) {
+                //    cv::Mat currentMask1 = cv::Mat::zeros(cv::Size(55, 70), CV_8UC1);
+                //    cv::line(currentMask1, cv::Point(0, 70), cv::Point(55, 0), cv::Scalar(255), 3);
+                //    addLineDefect(inputBatchesDefects, i, j, 200, 1500, 55, 70, "O.2.3", currentMask1);
 
-                    cv::Mat currentMask2 = cv::Mat::zeros(cv::Size(25, 60), CV_8UC1);
-                    cv::line(currentMask2, cv::Point(0, 0), cv::Point(25, 60), cv::Scalar(255), 3);
-                    addHangingSting(inputBatchesDefects, i, j, 470, 1500, 25, 60, "O.2.3", currentMask2);
-                }
-                if (i == 2 && j == 0) {
-                    cv::Mat currentMask = cv::Mat::zeros(cv::Size(215, 70), CV_8UC1);
-                    cv::line(currentMask, cv::Point(0, 70), cv::Point(115, 0), cv::Scalar(255), 3);
-                    cv::line(currentMask, cv::Point(115, 0), cv::Point(215, 70), cv::Scalar(255), 3);
-                    addHangingSting(inputBatchesDefects, i, j, 255, 1430, 215, 70, "O.2.3", currentMask);
-                }
-                if (i == 3 && j == 1) {
-                    cv::Mat currentMask = cv::Mat::zeros(cv::Size(35, 80), CV_8UC1);
-                    cv::line(currentMask, cv::Point(0, 0), cv::Point(35, 80), cv::Scalar(255), 3);
-                    addHangingSting(inputBatchesDefects, i, j, 505, 1580, 35, 80, "O.2.3", currentMask);
-                }
+                //    cv::Mat currentMask2 = cv::Mat::zeros(cv::Size(25, 60), CV_8UC1);
+                //    cv::line(currentMask2, cv::Point(0, 0), cv::Point(25, 60), cv::Scalar(255), 3);
+                //    addLineDefect(inputBatchesDefects, i, j, 470, 1500, 25, 60, "O.2.3", currentMask2);
+                //}
+                //if (i == 2 && j == 0) {
+                //    cv::Mat currentMask = cv::Mat::zeros(cv::Size(215, 70), CV_8UC1);
+                //    cv::line(currentMask, cv::Point(0, 70), cv::Point(115, 0), cv::Scalar(255), 3);
+                //    cv::line(currentMask, cv::Point(115, 0), cv::Point(215, 70), cv::Scalar(255), 3);
+                //    addLineDefect(inputBatchesDefects, i, j, 255, 1430, 215, 70, "O.2.3", currentMask);
+                //}
+                //if (i == 3 && j == 1) {
+                //    cv::Mat currentMask = cv::Mat::zeros(cv::Size(35, 80), CV_8UC1);
+                //    cv::line(currentMask, cv::Point(0, 0), cv::Point(35, 80), cv::Scalar(255), 3);
+                //    addLineDefect(inputBatchesDefects, i, j, 502, 1575, 35, 80, "O.2.3", currentMask);
+                //}
 
-                // Батч (0, 0) - линия от (410, 410) до (490, 490)
-                if (i == 0 && j == 0) {
-                    cv::Mat mask = cv::Mat::zeros(cv::Size(90, 90), CV_8UC1);
-                    cv::line(mask, cv::Point(0, 0), cv::Point(80, 80), cv::Scalar(255), 5);  // Линия от (0, 0) до (80, 80)
-                    addHangingSting(inputBatchesDefects, i, j, 410, 410, 90, 90, "O.2.3", mask);
-                }
+                //// Батч (0, 0) - линия от (410, 410) до (490, 490)
+                //if (i == 0 && j == 0) {
+                //    cv::Mat mask = cv::Mat::zeros(cv::Size(90, 90), CV_8UC1);
+                //    cv::line(mask, cv::Point(0, 0), cv::Point(80, 80), cv::Scalar(255), 5);  // Линия от (0, 0) до (80, 80)
+                //    addLineDefect(inputBatchesDefects, i, j, 410, 410, 90, 90, "O.2.3", mask);
+                //}
 
-                // Батч (1, 1) - линия от (500, 500) до (1000, 1000)
-                if (i == 1 && j == 1) {
-                    cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
-                    cv::line(mask, cv::Point(0, 0), cv::Point(500, 500), cv::Scalar(255), 5);  // Линия от (0, 0) до (500, 500)
-                    addHangingSting(inputBatchesDefects, i, j, 500, 500, 500, 500, "O.2.3", mask);
-                }
+                //// Батч (1, 1) - линия от (500, 500) до (1000, 1000)
+                //if (i == 1 && j == 1) {
+                //    cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
+                //    cv::line(mask, cv::Point(0, 0), cv::Point(500, 500), cv::Scalar(255), 5);  // Линия от (0, 0) до (500, 500)
+                //    addLineDefect(inputBatchesDefects, i, j, 500, 500, 500, 500, "O.2.3", mask);
+                //}
 
-                // Батч (2, 2) - линия от (1500, 1000) до (1000, 1500)
-                if (i == 2 && j == 2) {
-                    cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
-                    cv::line(mask, cv::Point(500, 0), cv::Point(0, 500), cv::Scalar(255), 5);  // Линия от (0, 0) до (500, 500)
-                    addHangingSting(inputBatchesDefects, i, j, 1000, 1000, 500, 500, "O.2.3", mask);
-                }
+                //// Батч (2, 2) - линия от (1500, 1000) до (1000, 1500)
+                //if (i == 2 && j == 2) {
+                //    cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
+                //    cv::line(mask, cv::Point(500, 0), cv::Point(0, 500), cv::Scalar(255), 5);  // Линия от (0, 0) до (500, 500)
+                //    addLineDefect(inputBatchesDefects, i, j, 1000, 1000, 500, 500, "O.2.3", mask);
+                //}
 
-                // Нитки "галочкой"
-                if (i == 4 && j == 0) {
-                    cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
-                    cv::line(mask1, cv::Point(100, 0), cv::Point(0, 100), cv::Scalar(255), 5); 
-                    addHangingSting(inputBatchesDefects, i, j, 0, 2000, 100, 100, "O.2.3", mask1);
+                //// Нитки "галочкой"
+                //if (i == 4 && j == 0) {
+                //    cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
+                //    cv::line(mask1, cv::Point(100, 0), cv::Point(0, 100), cv::Scalar(255), 5);
+                //    addLineDefect(inputBatchesDefects, i, j, 0, 2000, 100, 100, "O.2.3", mask1);
 
-                    cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
-                    cv::line(mask2, cv::Point(0, 0), cv::Point(100, 100), cv::Scalar(255), 5); 
-                    addHangingSting(inputBatchesDefects, i, j, 105, 2000, 100, 100, "O.2.3", mask2);
-                }
+                //    cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
+                //    cv::line(mask2, cv::Point(0, 0), cv::Point(100, 100), cv::Scalar(255), 5);
+                //    addLineDefect(inputBatchesDefects, i, j, 105, 2000, 100, 100, "O.2.3", mask2);
+                //}
 
-                // Нитки, пересекающиеся ровно на левом краю полотна и совпадающие
-                if (i == 0 && j == 0) {
-                    cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
-                    cv::line(mask1, cv::Point(0, 0), cv::Point(100, 100), cv::Scalar(255), 5);
-                    addHangingSting(inputBatchesDefects, i, j, 0, 108, 100, 100, "O.2.3", mask1);
+                //// Нитки, пересекающиеся ровно на левом краю полотна и совпадающие
+                //if (i == 0 && j == 0) {
+                //    cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
+                //    cv::line(mask1, cv::Point(0, 0), cv::Point(100, 100), cv::Scalar(255), 5);
+                //    addLineDefect(inputBatchesDefects, i, j, 0, 108, 100, 100, "O.2.3", mask1);
 
-                    cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
-                    cv::line(mask2, cv::Point(100, 0), cv::Point(0, 100), cv::Scalar(255), 5);
-                    addHangingSting(inputBatchesDefects, i, j, 0, 0, 100, 100, "O.2.3", mask2);
+                //    cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
+                //    cv::line(mask2, cv::Point(100, 0), cv::Point(0, 100), cv::Scalar(255), 5);
+                //    addLineDefect(inputBatchesDefects, i, j, 0, 0, 100, 100, "O.2.3", mask2);
 
-                    cv::Mat mask3 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
-                    cv::line(mask3, cv::Point(0, 0), cv::Point(100, 100), cv::Scalar(255), 5);
-                    addHangingSting(inputBatchesDefects, i, j, 50, 150, 100, 100, "O.2.3", mask3);
-                }
+                //    cv::Mat mask3 = cv::Mat::zeros(cv::Size(100, 100), CV_8UC1);
+                //    cv::line(mask3, cv::Point(0, 0), cv::Point(100, 100), cv::Scalar(255), 5);
+                //    addLineDefect(inputBatchesDefects, i, j, 50, 150, 100, 100, "O.2.3", mask3);
+                //}
 
-                // Примеры рассечки
-                if (i == 0 && j <= 4) {
-                    addDefect(inputBatchesDefects, i, j, 1800, 20 + 500 * j, 20, 480, "T.2.2.1");
-                }
+                //// Примеры рассечки
+                //if (i == 0 && j <= 4) {
+                //    addDefect(inputBatchesDefects, i, j, 1800, 20 + 500 * j, 20, 480, "T.2.2.1");
+                //}
 
+                 // Пример для вертикального дефекта типа "Т.1.1" в первой строке
+                 if (i == 0 && j == 0) {
+                 // Первая маска
+                 cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                 cv::line(mask1, cv::Point(90, 0), cv::Point(90, 500), cv::Scalar(255), 5);
+                 addLineDefect(inputBatchesDefects, i, j, 0, 0, 100, 500, "T.1.1", mask1);
+
+                 // Вторая маска
+                 cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                 cv::line(mask2, cv::Point(0, 0), cv::Point(0, 500), cv::Scalar(255), 5);
+                 addLineDefect(inputBatchesDefects, i, j, 105, 0, 100, 500, "T.1.1", mask2);
+                 }
+
+                 // Пример для вертикального дефекта типа "Т.1.1" во второй строке
+                 if (i == 0 && j == 1) {
+                     // Первая маска
+                     cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                     cv::line(mask1, cv::Point(90, 0), cv::Point(90, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 0, 500, 100, 500, "T.1.1", mask1);
+
+                     // Вторая маска
+                     cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                     cv::line(mask2, cv::Point(0, 0), cv::Point(0, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 105, 500, 100, 500, "T.1.1", mask2);
+                 }
+
+                 // Пример для вертикального дефекта типа "Т.1.1" в третьей строке
+                 if (i == 0 && j == 2) {
+                     // Первая маска
+                     cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                     cv::line(mask1, cv::Point(90, 0), cv::Point(90, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 0, 1000, 100, 500, "T.1.1", mask1);
+
+                     // Вторая маска
+                     cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                     cv::line(mask2, cv::Point(0, 0), cv::Point(0, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 105, 1000, 100, 500, "T.1.1", mask2);
+                 }
+
+                 // Пример для вертикального дефекта типа "Т.1.1" в четвертой строке
+                 if (i == 0 && j == 3) {
+                     // Первая маска
+                     cv::Mat mask1 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                     cv::line(mask1, cv::Point(90, 0), cv::Point(90, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 0, 1500, 100, 500, "T.1.1", mask1);
+
+                     // Вторая маска
+                     cv::Mat mask2 = cv::Mat::zeros(cv::Size(100, 500), CV_8UC1); // Уменьшаем размер до 100x500
+                     cv::line(mask2, cv::Point(0, 0), cv::Point(0, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 105, 1500, 100, 500, "T.1.1", mask2);
+                 }
+
+
+                 // Пример для вертикального дефекта типа "Т.1.1" в первой строке
+                 if (i == 3 && j == 0) {
+                     cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
+                     cv::line(mask, cv::Point(400, 0), cv::Point(400, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 1500, 0, 500, 500, "T.1.1", mask);
+                 }
+
+                 // Пример для вертикального дефекта типа "Т.1.1" во второй строке
+                 if (i == 3 && j == 1) {
+                     cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
+                     cv::line(mask, cv::Point(400, 0), cv::Point(400, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 1500, 500, 500, 500, "T.1.1", mask);
+                 }
+
+                 // Пример для вертикального дефекта типа "Т.1.1" в третьей строке
+                 if (i == 3 && j == 2) {
+                     cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
+                     cv::line(mask, cv::Point(400, 0), cv::Point(400, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 1500, 1000, 500, 500, "T.1.1", mask);
+                 }
+
+                 // Пример для вертикального дефекта типа "Т.1.1" в четвертой строке
+                 if (i == 3 && j == 3) {
+                     cv::Mat mask = cv::Mat::zeros(cv::Size(500, 500), CV_8UC1);
+                     cv::line(mask, cv::Point(400, 0), cv::Point(400, 500), cv::Scalar(255), 5);
+                     addLineDefect(inputBatchesDefects, i, j, 1500, 1500, 500, 500, "T.1.1", mask);
+                 }
             }
         }
     }
@@ -269,12 +350,6 @@ int main()
         //базовая функция для тестирования
         //mergeDefects(std::move(inputBatchesDefects), resultDefects);  // используем move, чтобы не копировать входные дефекты в функцию, а переместить их
 
-        // Отображение всех дефектов на одном изображении
-        cv::Mat combinedImage = displayDefects(resultDefects);
-        // Выводим результаты
-        cv::imshow("Combined Defects", combinedImage);
-        cv::waitKey(0);
-        cv::destroyAllWindows();
 
         for (const auto& defect : resultDefects)
         {
@@ -282,7 +357,14 @@ int main()
                 << ", Rect: (" << defect.rect.x << ", " << defect.rect.y << ", "
                 << defect.rect.width << ", " << defect.rect.height << ")"
                 << ", Probability: " << defect.prob << std::endl;
-        }    
+        }
+
+        // Отображение всех дефектов на одном изображении
+        cv::Mat combinedImage = displayDefects(resultDefects);
+        // Выводим результаты
+        cv::imshow("Combined Defects", combinedImage);
+        cv::waitKey(0);
+        cv::destroyAllWindows();
     }
     return 0;
 }
